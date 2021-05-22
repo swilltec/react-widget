aa508419c3ad0ef5b8c67c5ac771b41ff54a2ea2
swill <swilltec@gmail.com>
2021-05-22 09:12:39 +0000

    style: add sementic ui

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-export const Accordion = () => {
-    return (
-        <div>
-            Accordion
-        </div>
-    )
-}
+export const Accordion = ({ items }) => {
+  return (
+    <div>
+      {items.map((item) => {
+        return (
+          <div key={item.id}>
+            <div className="title active">
+              <i className="dropdown icon"></i>
+              {item.title}
+            </div>
+            <div className="content active">{item.content}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,44 @@
-import React from 'react'
-import { Accordion } from './Accordion'
+import React from "react";
+import { Accordion } from "./Accordion";
+
+const items = [
+  {
+      id: 1,
+    title: "Mention what is the file extension of JSON?",
+    content: "File extension of JSON is .json",
+  },
+  {
+    id: 2,
+    title: "Mention what are the data types supported by JSON?",
+    content: `Data types supported by JSON includes: Number, String
+            Boolean, Array, Object, Null`,
+  },
+  {
+    id: 3,
+    title: "Mention what is the role of JSON.stringify?",
+    content: "JSON.stringify() converts an object into a JSON text and saves that JSON text in a string.",
+  },
+  {
+    id: 3,
+    title: " Mention what is the function used for encoding JSON in PHP?",
+    content: "For encoding JSON in PHP, json_encode () function is used.  This function returns the JSON representation of a value on success or false on failure.",
+  },
+  {
+    id: 4,
+    title: "Explain how you can convert a string into a JSON Array?",
+    content: "To convert a string into a JSON array, you need to create a JSONObject object for each of your objects, and add those to your JSON array.",
+  },
+  {
+    id: 5,
+    title: "Mention what is the MIME type of JSON?",
+    content: "MIME type for JSON text is “application/json”",
+  },
+];
 
 export const App = () => {
-    return (
-        <div>
-            <Accordion />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
+};
